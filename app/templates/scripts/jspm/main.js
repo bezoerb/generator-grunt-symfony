45296ecd6c2eb00f64f180b81a775901b7301814
Ben Zörb <ben@sommerlaune.com>
2015-11-09 23:24:50 +0100

    better es2015 style

--- app/templates/scripts/jspm/main.js
+++ app/templates/scripts/jspm/main.js
@@ -7,10 +7,9 @@ import picturefill from  'picturefill';
 
 picturefill();
 
-
 log.setLevel(0);
 log.debug('\'Allo \'Allo');
 log.debug('Running jQuery:',$().jquery);<% if (useBootstrap) { %>
-log.debug('Running Bootstrap:',!!$.fn.scrollspy? '~3.3.0' : false);<% } else if (useUikit) { %>
+log.debug('Running Bootstrap:',Boolean($.fn.scrollspy)? '~3.3.0' : false);<% } else if (useUikit) { %>
 log.debug('Running UIkit:',UI.version);<% } else if (useFoundation) { %>
 log.debug('Running Foundation:',Foundation.version);<% } %>
--- app/templates/scripts/webpack/main.js
+++ app/templates/scripts/webpack/main.js
@@ -11,7 +11,7 @@ picturefill();
 log.setLevel(0);
 log.debug('\'Allo \'Allo');
 log.debug('Running jQuery:',$().jquery);<% if (useBootstrap) { %>
-log.debug('Running Bootstrap:',!!$.fn.scrollspy? '~3.3.0' : false);<% } else if (useUikit) { %>
+log.debug('Running Bootstrap:',Boolean($.fn.scrollspy)? '~3.3.0' : false);<% } else if (useUikit) { %>
 log.debug('Running UIkit:',UI.version);<% } else if (useFoundation) { %>
 /* global Foundation */
 log.debug('Running Foundation:',Foundation.version);<% } %>
