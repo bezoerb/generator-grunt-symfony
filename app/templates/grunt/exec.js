c244e774fb402e897799c91299b15e2ec6c727b9
Ben Zörb <ben@sommerlaune.com>
2015-12-08 06:28:53 +0100

    Symfony 3.0 compatibility

--- app/templates/grunt/exec.js
+++ app/templates/grunt/exec.js
@@ -1,5 +1,5 @@
 'use strict';
 module.exports = {
-    sfcl: 'php app/console cache:clear'<% if (useJspm) { %>,
+    sfcl: 'php <% if (parseFloat(symfonyDistribution.commit) >= 3 ) { %>bin<% } else { %>app<% } %>/console cache:clear'<% if (useJspm) { %>,
     jspm: 'node_modules/.bin/jspm bundle-sfx scripts/main .tmp/scripts/main.js'<% } %>
 };
