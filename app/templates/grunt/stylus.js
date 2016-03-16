4f84058a9074aa3f9533de3e84e9dbc1a31ec921
Ben Zörb <ben@sommerlaune.com>
2016-03-16 16:42:18 +0100

    fix paths for bootstrap-stylus

--- app/templates/grunt/stylus.js
+++ app/templates/grunt/stylus.js
@@ -5,7 +5,7 @@ module.exports = {
             '.tmp/stylus/main.css': '<%%= paths.app %>/styles/main.styl'
         },
         options: {
-            paths: ['bower_components'<% if (frameworkFromNpm) { %>, 'node_modules'<% if (useBootstrap) { %>, 'node_modules/bootstrap-styl'<% }} %>],
+            paths: ['bower_components'<% if (frameworkFromNpm) { %>, 'node_modules'<% if (useBootstrap) { %>, 'node_modules/bootstrap-styl'<% }} else if (useBootstrap) { %>, 'bower_components/bootstrap-stylus'<% } %>],
             'include css':true,
             use: [require('nib')],
             dest: '.tmp'
